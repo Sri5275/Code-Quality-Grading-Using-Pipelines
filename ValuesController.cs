e43aee9e88d3ba9680c5ce8dd87ddcd750368123
Saketh1844 <107761612+Sri5275@users.noreply.github.com>
2024-03-23 11:54:50 +0530

    Create ValuesController.cs

---
+++ ValuesController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from ASP.NET Core Web API!";
+        }
+    }
+}
